e585fdda63ea96bf7f68cd0658c66bcc1d4ce947
sharifzodah <sharifzodah@gmail.com>
2024-10-21 15:16:54 -0400

    excel demo file added

--- ExcelDemo.js
+++ ExcelDemo.js
@@ -1,19 +1,37 @@
 const ExceJS = require('exceljs');
 
-async function excelTest() 
-{
+async function writeExcel(searchText, newText, changeCoord, filePath) 
+{    
     const workbook = new ExceJS.Workbook();
-    await workbook.xlsx.readFile('D:\\Downloads - 2017\\Exceldownload.xlsx');
+    await workbook.xlsx.readFile(filePath);
     const worksheet = workbook.getWorksheet('Sheet1');
+    output = await readExcel(worksheet, searchText);
+    const cell = worksheet.getCell(output.row, output.column + changeCoord.colChange);
+    cell.value = newText;
+    await workbook.xlsx.writeFile(filePath)
+}
+
+async function readExcel(worksheet, searchText)
+{
+    let output = {row:-1, column:-1};
     worksheet.eachRow((row, rowNumber) =>
         {
             row.eachCell((cell, colNumber) =>
                 {
-                    console.log(cell.value);
+                    if(cell.value === searchText)
+                    {
+                        output.row = rowNumber;
+                        output.column = colNumber;
+                    }
                 })
 
         })
+        return output;    
 }
 
-excelTest();
+const searchText = 'Republic';
+const newText = 9000;
+const filePath = 'D:\\Downloads - 2017\\Exceldownload.xlsx';
+const changeCoord = {rowChange: 0, colChange: 2};
+writeExcel(searchText, newText, changeCoord, filePath);
 
